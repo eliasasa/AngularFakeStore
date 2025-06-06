a917fee9ecb5e890c24976f934eea2e196f77bf1
eliasasa <eliasnetoneto1@hotmail.com>
2025-06-05 20:17:18 -0400

    banner mais bonito

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { NgZone } from '@angular/core';
 
@@ -8,7 +8,7 @@ import { NgZone } from '@angular/core';
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements AfterViewInit, OnDestroy {
+export class BannerComponent implements AfterViewInit {
   bannerImages = [
     'assets/images/banner/banner-first.jpg',
     'assets/images/banner/banner-second.jpg',
@@ -16,8 +16,10 @@ export class BannerComponent implements AfterViewInit, OnDestroy {
   ];
 
   currentSrc = this.bannerImages[0];
-  private currentIndex = 0;
+  protected currentIndex = 0;
   private interval: any;
+  isFading = false;
+  private fadeDuration = 300;
   
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private ngZone: NgZone) {}
 
@@ -28,11 +30,27 @@ export class BannerComponent implements AfterViewInit, OnDestroy {
     this.ngZone.runOutsideAngular(() => {
       this.interval = setInterval(() => {
         this.ngZone.run(() => {
-          this.currentIndex = (this.currentIndex + 1) % this.bannerImages.length;
-          this.currentSrc = this.bannerImages[this.currentIndex];
+          this.fadeToImage((this.currentIndex + 1) % this.bannerImages.length);
         });
-      }, 3000);
+      }, 5000);
     });
+  }
+
+  fadeToImage(index: number) {
+    if (this.isFading) return;
+    this.isFading = true;
+    setTimeout(() => {
+      this.currentIndex = index;
+      this.currentSrc = this.bannerImages[this.currentIndex];
+      setTimeout(() => {
+        this.isFading = false;
+      }, this.fadeDuration);
+    }, this.fadeDuration);
+  }
+
+  goToImage(index: number) {
+    if (index === this.currentIndex || this.isFading) return;
+    this.fadeToImage(index);
   }
 
   ngAfterViewInit() {
