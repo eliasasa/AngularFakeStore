25c2d6a8c791ac9abf95ec16104384f5e7e5a318
eliasasa <eliasnetoneto1@hotmail.com>
2025-06-08 22:07:47 -0400

    add card component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,23 +1,46 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, AfterViewInit} from '@angular/core';
 import { EnviarFormService } from '../../services/enviar-form.service';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { Filter, FilterContent } from '../../components/filter/filter';
+import { ProductCard } from '../../components/product-card/product-card';
+
 
 @Component({
   selector: 'app-home',
   imports: [
     NavbarComponent,
     BannerComponent,
-    Filter
+    Filter,
+    ProductCard,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   filters: FilterContent[] = [
     { title: 'Categoria', value: 'categoria', materialIcon: 'category' },
     { title: 'Preço', value: 'preco', materialIcon: 'attach_money' },
     { title: 'Avaliação', value: 'avaliacao', materialIcon: 'star_rate' }
   ];
+
+
+  @ViewChild('filterComp') filterComponent!: Filter;  
+
+  getSelectedFilter() {
+    return this.filterComponent?.getSelectedFilterValue();
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  x : string | null = null;
+
+  teste () {
+    this.x = this.getSelectedFilter();
+    window.alert(this.x);
+  }
+  
+
 }
