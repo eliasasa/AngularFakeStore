1a13ddabc751a735fa77946983a067d7278dade2
eliasasa <eliasnetoneto1@hotmail.com>
2025-06-10 22:41:02 -0400

    footer 100%, loginpage front 100%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    }
+    },
+    {
+      path: 'login',
+      component: LoginComponent  
+    },
+    // {
+    //   path: '**',
+    //   component: Error  
+    // },
 ];
---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
