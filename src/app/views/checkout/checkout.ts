6a174054937e079156eaa25031282dbbdcba8b2c
eliasasa <eliasnetoneto28@gmail.com>
2026-01-11 21:34:04 -0400

    feat(checkout): enhance checkout payment validation and pricing logic

--- src/app/views/checkout/checkout.ts
+++ src/app/views/checkout/checkout.ts
@@ -3,15 +3,17 @@ import { CartService } from '../../services/cart/cart-service';
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth/auth-service';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { User } from '../../services/user/user';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ToastService } from '../../services/toast/toast-service';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
+type FreteTipo = 'PAC' | 'JADLOG' | 'SEDEX' | 'LOGGI';
+
 @Component({
   selector: 'app-checkout',
-  imports: [DecimalPipe, CommonModule, RouterLink, ReactiveFormsModule, NgxMaskDirective, NgxMaskPipe],
+  imports: [DecimalPipe, CommonModule, RouterLink, ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './checkout.html',
   styleUrl: './checkout.scss',
 })
@@ -23,14 +25,29 @@ export class Checkout implements OnInit {
     private user: User,
     private fb: FormBuilder,
     private toast: ToastService,
+    private router: Router
   ) {}
 
   total: number = 0;
+
+  FRETES: Record<FreteTipo, number> = {
+    PAC: 19.90,
+    JADLOG: 24.90,
+    SEDEX: 29.90,
+    LOGGI: 34.90
+  };
+
+  JUROS_MENSAL = 0.029;
+  PARCELAS_SEM_JUROS = 3;
+
   currentStep: number = 2;
   isLogged: boolean = false
   private authSub!: Subscription;
   userData: any = null;
   isCvvFocused = false;
+  boletoGerado = false;
+  pixGerado = false;
+  compraEfetuada = false;
 
   personalForm!: FormGroup;
   addressForm!: FormGroup;
@@ -75,6 +92,10 @@ export class Checkout implements OnInit {
           installments: ['1']
         });
 
+        this.paymentForm.get('paymentMethod')?.valueChanges.subscribe(method => {
+          this.updatePaymentValidators(method);
+        });
+
         this.user.getInfoUser(userId).then(res => {
           this.userData = res;
 
@@ -105,14 +126,94 @@ export class Checkout implements OnInit {
     }
 
   }
-  
+
+  finalizarCompra() {
+    this.paymentForm.markAllAsTouched();
+    if (!this.currentFormValid()) return;
+
+    if (this.compraEfetuada) {
+      this.router.navigate(['/']);
+      this.toast.showToast('Compra realizada com sucesso!', 'sucesso');
+      this.cart.cleanCart();
+      return;
+    }
+
+    if (this.paymentMethod === 'TICKET') {
+      this.toast.showToast('Boleto gerado com sucesso!', 'sucesso');
+      this.boletoGerado = true;
+      this.compraEfetuada = true;
+      return;
+    }
+
+    if (this.paymentMethod === 'PIX') {
+      if (this.pixGerado) {
+        this.compraEfetuada = true;
+      } else {
+        this.toast.showToast('Gere o QRCode antes de tentar finalizar a compra.', 'erro')
+      }
+      return;
+    }
+
+    if (this.paymentMethod === 'CREDIT' || this.paymentMethod === 'DEBIT') {
+      this.toast.showToast('Compra realizada com sucesso!', 'sucesso');
+      this.cart.cleanCart();
+      this.router.navigate(['/']);
+      return;
+    }
+  }
+
+  openFakeBoleto() {
+    window.open('assets/images/checkout/boleto.jpeg', '_blank');
+  }
+
   get paymentMethod() {
     return this.paymentForm.get('paymentMethod')?.value;
   }
 
+  updatePaymentValidators(method: string) {
+    const cardControls = ['cardNumber', 'cardName', 'cardExpiry', 'cardCvv'];
+
+    cardControls.forEach(control => {
+      const field = this.paymentForm.get(control);
+
+      field?.reset();
+
+      field?.clearValidators();
+    });
+
+    if (method === 'CREDIT' || method === 'DEBIT') {
+      this.paymentForm.get('cardNumber')?.setValidators([
+        Validators.required,
+        Validators.minLength(19)
+      ]);
+
+      this.paymentForm.get('cardName')?.setValidators([
+        Validators.required,
+        Validators.minLength(3)
+      ]);
+
+      this.paymentForm.get('cardExpiry')?.setValidators([
+        Validators.required
+      ]);
+
+      this.paymentForm.get('cardCvv')?.setValidators([
+        Validators.required,
+        Validators.minLength(3)
+      ]);
+    }
+
+    cardControls.forEach(control => {
+      this.paymentForm.get(control)?.updateValueAndValidity();
+    });
+
+    this.paymentForm.updateValueAndValidity();
+  }
+
   generatePixQr() {
+    this.compraEfetuada = true;
     this.pixQrCode =
-      'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=PIX-EXEMPLO';
+      'https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=' +
+      encodeURIComponent('https://github.com/eliasasa');
   }
 
   formatCard(value: string): string {
@@ -120,6 +221,60 @@ export class Checkout implements OnInit {
       ?.replace(/\D/g, '')
       .replace(/(.{4})/g, '$1 ')
       .trim();
+  }
+
+  get freteValor(): number {
+    const frete = this.addressForm.get('frete')?.value as FreteTipo | null;
+    return frete ? this.FRETES[frete] : 0;
+  }
+
+  get parcelas(): number {
+    return Number(this.paymentForm.get('installments')?.value ?? 1);
+  }
+
+  get valorParcela(): number {
+    if (this.paymentMethod !== 'CREDIT') return 0;
+
+    const base = this.valorFinal;
+
+    if (this.parcelas <= this.PARCELAS_SEM_JUROS) {
+      return Number((base / this.parcelas).toFixed(2));
+    }
+
+    const montante =
+      base * Math.pow(1 + this.JUROS_MENSAL, this.parcelas);
+
+    return Number((montante / this.parcelas).toFixed(2));
+  }
+
+
+  get valorFinal(): number {
+    if (!this.addressForm) return this.total;
+
+    let valor = this.total + this.freteValor;
+
+    switch (this.paymentMethod) {
+      case 'PIX':
+        valor *= 0.95;
+        break;
+
+      case 'CREDIT':
+        if (this.parcelas > this.PARCELAS_SEM_JUROS) {
+          valor *= Math.pow(1 + this.JUROS_MENSAL, this.parcelas);
+        }
+        break;
+    }
+
+    return Number(valor.toFixed(2));
+  }
+
+  valorParcelaComJuros(parcelas: number): number {
+    const total = this.total + this.freteValor;
+
+    const montante =
+      total * Math.pow(1 + this.JUROS_MENSAL, parcelas);
+
+    return montante / parcelas;
   }
 
   formatExpiration(value: string | number | null): string {
@@ -144,7 +299,6 @@ export class Checkout implements OnInit {
 
     return digits;
   }
-
 
   currentFormValid(): boolean {
     const form = this.getCurrentForm();
